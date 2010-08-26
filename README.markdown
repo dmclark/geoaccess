4802c03993e83c4099c3f0998a9a08cc8e5da1b0
David Clark <dmclark@gmail.com>
2010-08-25 20:33:28 -0400

    initial commit of Rails 3 application shell

---
+++ README.markdown
@@ -0,0 +1,5 @@
+== Welcome to Geolocation Accessibility Challenge  
+
+Prototype based on discussions from link:http://tech.groups.yahoo.com/group/geoaccess/
+
+More to come
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoaccess::Application.config.secret_token = '45379406066130221a854aee263c379e01c39f4ed6ae3c8051763a287615b2715a128ad0b4f16f95cd4e430313caa6249012cb6bce56edeee3dc4bbca17c0efa'
