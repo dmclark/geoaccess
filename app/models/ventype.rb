d162deb5598b4256e5a3d76d548212fb0d701bae
David Clark <dmclark@gmail.com>
2010-09-03 11:52:58 -0400

    annotate models

--- app/models/ventype.rb
+++ app/models/ventype.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+# Schema version: 20100902165058
+#
+# Table name: ventypes
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Ventype < ActiveRecord::Base
   has_many :venues
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+# Schema version: 20100902165058
+#
+# Table name: venues
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  address    :string(255)
+#  city       :string(255)
+#  state      :string(255)
+#  zip        :integer
+#  url        :string(255)
+#  is_active  :boolean
+#  ventype_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Venue < ActiveRecord::Base
   belongs_to :ventype, :class_name => "Ventype", :foreign_key => "ventype_id"
 end
