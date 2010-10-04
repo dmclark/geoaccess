033e34dc32d3855b3000931698d48e05386a21dc
David Clark <dmclark@gmail.com>
2010-10-04 12:32:32 -0400

    iwitial map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function initialize() {
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+  }
