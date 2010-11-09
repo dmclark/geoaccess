7659b30e476b5cf944429bd4d8c888907c4b7986
David Clark <dmclark@gmail.com>
2010-11-09 13:18:46 -0500

    goroo restclient firstpass

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,5 @@
 class Route < ActiveRecord::Base
-  attr_accessor :startpaint_id, :endpaint_id, :directions
+  attr_accessor :startpaint_id, :endpaint_id, :directions, :rta_directions
   
   belongs_to :startpoint, :class_name => "Venue", :foreign_key => "startpoint_id"
   belongs_to :endpoint, :class_name => "Venue", :foreign_key => "endpoint_id"
@@ -17,6 +17,10 @@ class Route < ActiveRecord::Base
         end 
   end
   
+  def rta_directions
+     Nokogiri::HTML(goroo_results).css('#tripPlan').text.html_safe
+  end
+  
   def distance
     Nokogiri::XML(parsed_xml).xpath('/DirectionsResponse/route/leg/distance/text').text.html_safe
   end
@@ -25,6 +29,35 @@ class Route < ActiveRecord::Base
   def parsed_xml
     GoogleDirections.new(startpoint.location, endpoint.location).xml
   end
+  
+  def goroo_results
+    RestClient.post 'http://goroo.com/goroo/showTripPlanResults.htm', 
+      {:params => {"calendarOpen" => "false", 
+          "calendarClick" => "false", 
+          "advanced" => "",
+          "newTrip" => "yes", 
+          "newRequest" => "true", 
+          "trip" => "0",
+          "dummy" => "", 
+          "origAddress" => startpoint.location,
+          "destAddress" => endpoint.location,
+          "arriveLeave" => "dep",
+
+          "travelDate" => "11/17/10",
+          "hour"  => "3",
+          "minute" => "00",
+          "ampm" => "pm",
+
+          "train" => "true", 
+          "bus"  => "true", 
+          "tripPreference"  => "LEASTTIME",
+
+          "walkPreference"  => "0.50",
+          "accessibleRequired"  => "true",
+          "takeMeThere" => "Get Public transit directions" }
+          }
+  end
+  
   
 
 private 
